4d8def4eb175311500a86c3cc7a10339deaaeeb0
Dan Blanchette <dlblanchette@students.nic.edu>
2024-03-04 15:48:47 -0800

    Update treeUtils.cpp

--- ASN_3/treeUtils.cpp
+++ ASN_3/treeUtils.cpp
@@ -168,7 +168,7 @@ char *expTypeToStr(ExpType type, bool isArray, bool isStatic)
       break;
    }
    
-   sprintf(expBuff, "%s%s%s%s", (isStatic && isArray ? (char*)"static " : ""), (isArray ? (char *)"array of " : ""), (isArray && isStatic ? (char *)"static " : ""), exp_type_name);
+   sprintf(expBuff, "%s%s%s%s", (isStatic && isArray ? (char*)"static " : ""), (isArray && isStatic ? (char *)"static " : ""), (isArray ? (char *)"array of " : ""), exp_type_name);
    return expBuff; 
 }
 
