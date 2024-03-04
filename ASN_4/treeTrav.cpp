53b5e8d8d18f33ade4f5b6ab96979d351bf55bd6
Dan Blanchette <dlblanchette@students.nic.edu>
2024-03-04 14:25:10 -0800

    created treeTrav.cpp for assignment 4

--- ASN_4/treeTrav.cpp
+++ ASN_4/treeTrav.cpp
@@ -1,5 +1,14 @@
 void treeTraverseDecl(TreeNode *syntree, SymblTable *symtab)
 {
    // debug statement here
-   
+   TreeNode * c0,  *c1, *temp;
+   c0 = syntree->child[0];
+   c1 = syntree->child[1];
+
+   switch(syntree->kind.decl)
+   {
+      case FunK:
+         // debug goes here
+         int foffset = -2;
+   }
 }
