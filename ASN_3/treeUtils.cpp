d66496bd1e0fc1f0623e1b6a7435bd045efa833e
Dan Blanchette <dlblanchette@students.nic.edu>
2024-03-04 17:16:15 -0800

    Revert "Update treeUtils.cpp"
    This reverts commit 432a2e53474e6425147c9fb531e6fccf8d4699e9.

--- ASN_3/treeUtils.cpp
+++ ASN_3/treeUtils.cpp
@@ -171,19 +171,19 @@ char *expTypeToStr(ExpType type, bool isArray, bool isStatic)
       switch(type)
       {
          case ExpType::Boolean:
-            return (char *)"static array of type bool";
+            return (char *)"array of type bool";
             break;
          case ExpType::Char:
-            return (char *)"static array of type char";
+            return (char *)"array of type char";
             break;
          case ExpType::Integer:
-            return (char *)"static array of type int";
+            return (char *)"array of type int";
             break;
          case ExpType::UndefinedType:
-            return (char *)"static array of type UNDEFINED";
+            return (char *)"array of type UNDEFINED";
             break;
          case ExpType::Void:
-            return (char *)"static array of type void";
+            return (char *)"array of type void";
             break;
       }      
    }
