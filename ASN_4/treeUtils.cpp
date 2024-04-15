8bf14a7324074433ba1c3e7d8b82329cde28d282
Dan Blanchette <dlblanchette@students.nic.edu>
2024-04-14 17:36:48 -0700

    Update treeUtils.cpp

--- ASN_4/treeUtils.cpp
+++ ASN_4/treeUtils.cpp
@@ -411,7 +411,7 @@ void printTreeNode(FILE *out, TreeNode *syntaxTree, bool showExpType, bool showA
       fprintf(out, "hey I'm a node, say something here.", syntaxTree->nodekind);
    }
 
-   if (showAllocation && (syntaxTree->varKind != VarKind::None || syntaxTree->size != 1) && !ExpKind::CallK)
+   if (showAllocation && (syntaxTree->varKind != VarKind::None || syntaxTree->size != 1 || !ExpKind::CallK ))
    {
       // printf("Entered MemeAllloc\n");
       // updated to fprintf (might be the issue as it was not using the filestream object originally printf())
