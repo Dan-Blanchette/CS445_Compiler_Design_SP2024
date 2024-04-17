dc848f0ad23a25877d7130f700be286b310f0b68
Dan Blanchette <dlblanchette@students.nic.edu>
2024-04-17 15:20:02 -0700

    fixing bad logic

--- ASN_4/treeUtils.cpp
+++ ASN_4/treeUtils.cpp
@@ -413,7 +413,7 @@ void printTreeNode(FILE *out, TreeNode *syntaxTree, bool showExpType, bool showA
       fprintf(out, "Unknow Node Type", syntaxTree->nodekind);
    }
 
-   if (showAllocation && (syntaxTree->varKind != VarKind::None || syntaxTree->size != 1 || syntaxTree->kind.stmt != StmtKind::CompoundK))
+   if (showAllocation && (syntaxTree->varKind != VarKind::None || syntaxTree->size != 1))
    {
       // printf("Entered MemeAllloc\n");
       // updated to fprintf (might be the issue as it was not using the filestream object originally printf())
