694b9c975be1a07b454ce52ca0b820803975a3c1
Dan Blanchette <dlblanchette@students.nic.edu>
2024-05-09 12:33:32 -0700

    Update codegen.cpp

--- ASN_5/codegen.cpp
+++ ASN_5/codegen.cpp
@@ -196,12 +196,6 @@ void codegenExpression(TreeNode *currentNode)
                   emitRO((char *)"MOV", 4, 3, 5, (char *)"array op =");
                }
             }
-            else
-            {
-               //printf("I made it to the ELSE: =======================\n");
-               emitRM((char *)"LDC", AC, currentNode->child[1]->attr.value, 6, (char *)"Load integer constant");
-               emitRM((char *)"ST", AC, currentNode->child[0]->offset, FP, (char *)"Store variable", currentNode->child[0]->attr.name);
-            }
          }
 
          else
@@ -245,6 +239,12 @@ void codegenExpression(TreeNode *currentNode)
             }
             emitRO((char *)"LDA", AC, 1, 3, (char *)"increment value of", currentNode->child[0]->attr.name);
          }
+      }
+      else
+      {
+         //printf("I made it to the ELSE: =======================\n");
+         emitRM((char *)"LDC", AC, currentNode->child[1]->attr.value, 6, (char *)"Load integer constant");
+         emitRM((char *)"ST", AC, currentNode->child[0]->offset, FP, (char *)"Store variable", currentNode->child[0]->attr.name);
       }
       // AssignK case break
       break;
@@ -346,21 +346,21 @@ void codegenExpression(TreeNode *currentNode)
          switch (currentNode->attr.op)
          {
             case '+':
-               emitRO((char *)"OP", 3, 4, 3, (char *)'+');
+               emitRO((char *)"OP", 3, 4, 3, (char *)"+");
                // break out the case '+' statment
                break;
 
             case '-':
-               emitRO((char *)"OP", 3, 4, 3, (char *)'-');
+               emitRO((char *)"OP", 3, 4, 3, (char *)"-");
                break;
             case '=':
-               emitRO((char *)"OP", 3, 4, 3, (char *)'=');
+               emitRO((char *)"OP", 3, 4, 3, (char *)"=");
                break; 
             case '<':
-               emitRO((char *)"OP", 3, 4, 3, (char *)'<');
+               emitRO((char *)"OP", 3, 4, 3, (char *)"<");
                break;
             case '>':
-               emitRO((char *)"OP", 3, 4, 4, (char *)'>');
+               emitRO((char *)"OP", 3, 4, 4, (char *)">");
                break;
                
          }
