f5bb89da7fde609443bb41d78ad7879232622bf7
Dan Blanchette <dlblanchette@students.nic.edu>
2024-05-09 16:18:07 -0700

    Update semantics.cpp

--- ASN_5/semantics.cpp
+++ ASN_5/semantics.cpp
@@ -33,7 +33,7 @@ TreeNode *semanticAnalysis(TreeNode *syntree,          // pass in and return an 
    syntree = loadIOLib(syntree);
 
    // treeTraverse(syntree, symtabX); 
-
+   globalOffset = goffset;
    return syntree;
 }
 
